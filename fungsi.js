71595152be8ce0255532a4727070c6a56053d411
Riyan Muhammad Fajar <riyanmuhammad354@gmail.com>
2023-05-14 07:43:15 +0700

    menambahakn 2 file baru

--- fungsi.js
+++ fungsi.js
@@ -1,3 +1,16 @@
 console.log("fungsi Pertama");
 console.log("fungsi kedua");
 console.log("fungsi ketiga");
+console.log("makan Pertama");
+
+const penjumlahan = (a, b) => {
+  return parseInt(a + b);
+};
+
+console.log(penjumlahan(1, 2));
+
+function tambah(a, b) {
+  return a + b;
+}
+
+console.log(tambah(1, 4));
