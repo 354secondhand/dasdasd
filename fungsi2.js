01cd17dfc8d7429b5fd52305dc048bb7bea03da0
Riyan Muhammad Fajar <riyanmuhammad354@gmail.com>
2023-05-17 09:14:59 +0700

    menghapus data tidak perlu

--- fungsi2.js
+++
@@ -1,9 +0,0 @@
-const pengurangan = (a, b) => {
-  return a - b;
-};
-
-console.log(pengurangan(2, 3));
-console.log(pengurangan(2, 3));
-console.log(pengurangan(2, 3));
-console.log(pengurangan(2, 3));
-console.log(pengurangan(2, 3));
